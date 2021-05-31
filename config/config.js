d2609f69bd808fb49ea01d305f750ef962f52b0f
Yago Massiah <yagomassiah@gmail.com>
2021-05-31 00:25:03 -0300

    fixing config

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports = {
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
-        host: "127.0.0.1",
+        host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         dialect: "mysql",
     },
