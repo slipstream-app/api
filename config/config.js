2e4269f400fc4f3e6a7c8fcf94268e8c1e70211b
Yago Massiah <yagomassiah@gmail.com>
2021-05-30 22:43:58 -0300

    SMALL FIXES

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports = {
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
         host: "127.0.0.1",
-        port: 3307,
+        port: process.env.DB_PORT,
         dialect: "mysql",
     },
 };
